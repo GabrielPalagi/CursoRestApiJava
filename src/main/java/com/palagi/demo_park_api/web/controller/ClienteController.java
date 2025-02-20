cbd4c5648e7de3068e3ce5cda8e6ce8f171a3683
Gp <gabrielpalagi@gmail.com>
2025-02-20 20:56:22 -0300

    documentando getAll

--- src/main/java/com/palagi/demo_park_api/web/controller/ClienteController.java
+++ src/main/java/com/palagi/demo_park_api/web/controller/ClienteController.java
@@ -30,8 +30,6 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @Tag(name = "Clientes", description = "Contem todas as operacoes relativas aos recursos para cadastro,edicao e leitura de um cliente.")
 @RequiredArgsConstructor
 @RestController
